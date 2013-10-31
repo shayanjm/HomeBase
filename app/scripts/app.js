c3794984e286b997fa2430178103d968f6c9b7d1
Tigran Farmanyan <tigran.farmanyan@gmail.com>
2013-10-30 23:53:03 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('HomeBaseApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
